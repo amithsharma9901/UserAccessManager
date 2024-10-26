54a7ab055a18f5d44be16b6298def0be1d441a0e
amithsharma9901 <131507361+amithsharma9901@users.noreply.github.com>
2024-10-26 17:57:18 +0530

    Create j.java

---
+++ src/main/java/com/useraccessmanagement/servlets/j.java
@@ -0,0 +1,20 @@
+
+package com.useraccessmanagement.servlets;
+
+public class j {
+    private int id;
+    private String name;
+
+    public j(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
